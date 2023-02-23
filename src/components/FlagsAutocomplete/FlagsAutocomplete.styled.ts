81acfc93f2bc023fbd3a13f543577d635a596402
ja153903 <jaimeabbariao@gmail.com>
2023-02-22 23:17:50 -0500

    feat: remove light mode stuff from MUI example

--- src/components/FlagsAutocomplete/FlagsAutocomplete.styled.ts
+++ src/components/FlagsAutocomplete/FlagsAutocomplete.styled.ts
@@ -17,19 +17,16 @@ const Styled = {
       [`& .${autocompleteClasses.paper}`]: {
         boxShadow: 'none',
         margin: 0,
-        color: 'inherit',
-        fontSize: 13
+        color: 'inherit'
       },
       [`& .${autocompleteClasses.listbox}`]: {
-        backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#1c2128',
+        backgroundColor: '#fff',
         padding: 0,
         [`& .${autocompleteClasses.option}`]: {
           minHeight: 'auto',
           alignItems: 'flex-start',
           padding: 8,
-          borderBottom: `1px solid  ${
-            theme.palette.mode === 'light' ? ' #eaecef' : '#30363d'
-          }`,
+          borderBottom: `1px solid #eaecef`,
           '&[aria-selected="true"]': {
             backgroundColor: 'transparent'
           },
@@ -46,45 +43,29 @@ const Styled = {
   }),
   Popper: styled(Popper)(({ theme }) => {
     return {
-      border: `1px solid ${
-        theme.palette.mode === 'light' ? '#e1e4e8' : '#30363d'
-      }`,
-      boxShadow: `0 8px 24px ${
-        theme.palette.mode === 'light'
-          ? 'rgba(149, 157, 165, 0.2)'
-          : 'rgb(1, 4, 9)'
-      }`,
+      border: '1px solid #e1e4e8',
+      boxShadow: '0 8px 24px rgba(149, 157, 165, 0.2)',
       borderRadius: 6,
       width: 300,
       zIndex: theme.zIndex.modal,
-      fontSize: 13,
-      color: theme.palette.mode === 'light' ? '#24292e' : '#c9d1d9',
-      backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#1c2128'
+      color: '#24292e',
+      backgroundColor: '#fff'
     }
   }),
   Input: styled(InputBase)(({ theme }) => {
     return {
       padding: 10,
       width: '100%',
-      borderBottom: `1px solid ${
-        theme.palette.mode === 'light' ? '#eaecef' : '#30363d'
-      }`,
+      borderBottom: '1px solid #eaecef',
       '& input': {
         borderRadius: 4,
-        backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#0d1117',
+        backgroundColor: '#fff',
         padding: 8,
         transition: theme.transitions.create(['border-color', 'box-shadow']),
-        border: `1px solid ${
-          theme.palette.mode === 'light' ? '#eaecef' : '#30363d'
-        }`,
-        fontSize: 14,
+        border: '1px solid #eaecef',
         '&:focus': {
-          boxShadow: `0px 0px 0px 3px ${
-            theme.palette.mode === 'light'
-              ? 'rgba(3, 102, 214, 0.3)'
-              : 'rgb(12, 45, 107)'
-          }`,
-          borderColor: theme.palette.mode === 'light' ? '#0366d6' : '#388bfd'
+          boxShadow: '0px 0px 0px 3px rgba(3, 102, 214, 0.3)',
+          borderColor: '#0366d6'
         }
       }
     }
