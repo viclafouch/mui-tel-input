af04460ace0c40f3357c6b85887c8627cad7bd3a
ja153903 <jaimeabbariao@gmail.com>
2023-02-23 22:52:08 -0500

    feat: updating rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -39,9 +39,12 @@ export default defineConfig({
           '@mui/material/Menu': 'Menu',
           '@mui/material/MenuItem': 'MenuItem',
           '@mui/material/Typography': 'Typography',
+          '@mui/material/ListItem': 'ListItem',
           '@mui/material/ListItemIcon': 'ListItemIcon',
           '@mui/material/ListItemText': 'ListItemText',
           '@mui/material/colors': 'colors',
+          '@mui/material/Autocompelte': 'Autocomplete',
+          '@mui/material/ClickAwayListener': 'ClickAwayListener',
         }
       }
     }
